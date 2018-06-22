0bc570a7d02a6d54a10a29c26920b8d9ddda13f8
changhaoyin <dorisfenny30311@gmail.com>
2018-06-22 20:16:59 +0800

    Add files via upload

---
+++ view_minions/view_minionsTests/view_minionsTests.swift
@@ -0,0 +1,36 @@
+//
+//  view_minionsTests.swift
+//  view_minionsTests
+//
+//  Created by User10 on 2018/6/22.
+//  Copyright © 2018年 User10. All rights reserved.
+//
+
+import XCTest
+@testable import view_minions
+
+class view_minionsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
